8ce1cbfa0b90e0c24e2cc28e33fb308f7cd77db2
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-08-05 23:28:27 -0400

    Got the service worker and manifest at least partly working, working on bugs with offline availability now

--- Develop/webpack.config.js
+++ Develop/webpack.config.js
@@ -3,13 +3,28 @@ const path = require("path");
 
 const config = {
   // Update the entry point
-  entry: "./public/assets/js/app.js",
+  entry: "./public/index.js",
   output: {
     // Set the path and filename for the output bundle (hint: You will need to use "__dirname")
     path: path.join(__dirname, "./public/dist/"),
     filename: "bundle.js"
   },
-  mode: "production",
+  mode: "development",
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: "babel-loader",
+  //         options: {
+  //           presets: ["@babel/preset-env"]
+  //         }
+  //       }
+  //     }
+  //   ]
+  // },
+
   plugins: [new WebpackPwaManifest({
     name: 'Progressive Budget',
     short_name: 'Budget App',
@@ -21,7 +36,7 @@ const config = {
     crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
     icons: [
       {
-        src: path.resolve('./public/assets/images/icons/icon-512x512.png'),
+        src: path.resolve('./public/icons/icon-512x512.png'),
         sizes: [72, 96, 128, 144, 152, 192, 256, 384, 512] // multiple sizes
       }
     ],
@@ -31,14 +46,3 @@ const config = {
 };
 
 module.exports = config;
-
-// const config = {
-//   entry: "./src/app.js",
-//   output: {
-//     path: __dirname + "/dist",
-//     filename: "bundle.js"
-//   },
-//   mode: "development"
-// };
-
-// module.exports = config;
