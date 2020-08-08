6be82afb2c484f12ce314ec328964905702b0d59
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-08-08 14:31:48 -0400

    Working on getting Service Worker to allow offline functionality

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,3 +1,31 @@
-self.addEventListener("install", function(event) {
-    console.log("Installed", event);
-})
+console.log("WHEEEEEE")
+
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/styles/main.css',
+       '/scripts/main.min.js',
+       
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+   
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+});
+   
