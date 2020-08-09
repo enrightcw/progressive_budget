3c3d0eddea562d9b353577f08a7e14dd688d9e8e
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-08-08 23:31:21 -0400

    Still trying to sort out the service worker, and added a db file

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,31 +1,65 @@
-console.log("WHEEEEEE")
+const FILES_TO_CACHE = [
+    "./", 
+    "./index.html", 
+    "./style.css",
+    "./index.js", 
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
+    "../models/transaction.js",
+    "./db.js"
+];
 
-importScripts('/cache-polyfill.js');
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
+// install
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/styles/main.css',
-       '/scripts/main.min.js',
-       
-     ]);
-   })
- );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-   
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+// activate
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", evt => {
+  if (evt.request.url.includes("/api/transaction")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
     );
-});
-   
+
+    return;
+}});
